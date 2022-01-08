11a955488c047fc5d2986c957b2f2528b8d0a4f1
Guillaume Claverie <guillaume.claverie@mail.com>
2022-01-08 21:41:26 +0100

    Replace default platform name

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'esp32beaconplatform';
+export const PLATFORM_NAME = 'homebridge-esp32beacon';
 
 /**
  * This must match the name of your plugin as defined the package.json
